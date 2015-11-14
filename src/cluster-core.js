0b6c717f5debb875809d4f0313a82236efd2aada
frank@solygen.de <frank@solygen.de>
2015-11-14 19:05:25 +0100

    next version (hip)

--- src/cluster-core.js
+++ src/cluster-core.js
@@ -12,43 +12,46 @@ function extension(file) {
 
 module.exports = {
 
-    process: function (basepath) {
+    process: function (options) {
+        options = options || {};
+        if (!options.basepath) return;
 
-        var data = bfs(basepath),
+        var data = bfs(options.basepath),
             hash = {};
 
         // move files to subfolders
         (data.files).forEach(function (data) {
             var path = data.path,
                 file = data.name,
-                counter = 1;
+                counter = 1,
+                skipOnConflict = !options.rename && !options.overwrite;
 
             var stats = fs.statSync(path),
                 subdir = stats.mtime.toISOString().substr(0, 7),
-                dir = pt.join(basepath, subdir),
+                dir = pt.join(options.basepath, subdir),
                 target = pt.join(dir, file),
                 time = moment(stats.mtime);
 
-            var name = time.format('YYYY-MM-DD_HH-mm-ss') + '.' + extension(file);
-            target = pt.join(dir, name);
-
-            // add one second to filenames time part until no file will be overwritten
-            while (fs.existsSync(target)) {
-                time = time.add('s', 1);
-                name = time.format('YYYY-MM-DD_HH-mm-ss') + '.' + extension(file);
+            if (options.rename) {
+                var name = time.format('YYYY-MM-DD_HH-mm-ss') + '.' + extension(file);
                 target = pt.join(dir, name);
+                // fallback: add one second to filenames
+                while (fs.existsSync(target)) {
+                    time = time.add('s', 1);
+                    name = time.format('YYYY-MM-DD_HH-mm-ss') + '.' + extension(file);
+                    target = pt.join(dir, name);
+                }
             }
 
-            // create subdir
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir);
-            }
+            // skip
+            if (skipOnConflict && fs.existsSync(target)) return;
+
+            // create subdir when not existing
+            if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
             // rename/move
-            if (!fs.existsSync(target)) {
-                fs.renameSync(path, target);
-                hash[path] = target;
-            }
+            fs.renameSync(path, target);
+            hash[path] = target;
         });
 
         return hash;
--- src/cluster.js
+++ src/cluster.js
@@ -3,22 +3,40 @@
 
 (function () {
 
-    let inquirer = require('inquirer'),
+    let program = require('commander'),
+        inquirer = require('inquirer'),
         core = require('./cluster-core');
 
-    // first cli parameter or current directory
-    var base = process.argv[2] || process.cwd();
+    // define programm
+    program
+      .version('0.2.1')
+      .usage('<path (default:current path)> [options]')
+      .description('Clusters files in subdirectories based on month of creation')
+      .option('-r, --rename', 'also rename files [YYYY-MM-DD_HH-mm-ss]')
+      .option('-o, --overwrite', 'overwrite if file already exists (when rename is disabeld)')
+      //.option('-d, --details', 'show detailed output')
+      .option('-y, --yes', 'skip prompt')
+      .parse(process.argv);
 
-    function cont(answers) {
-        if (answers.process !== 'yes') return;
-        core.process(base);
-    }
+    // set options
+    var options = {
+            // parameter or current directory
+            basepath: program.args[0] || process.cwd(),
+            rename: program.rename,
+            overwrite: program.overwrite
+        };
+
+    // skip question
+    if (program.yes) return core.process(options);
 
     // ask the user to contiune
     inquirer.prompt([{
         type: 'list',
         name: 'process',
-        message: `Process with folder ${base} ?`,
+        message: `Process with folder ${options.basepath} ?`,
         choices: ['yes', 'no']
-    }], cont);
+    }], function cont(answers) {
+        if (answers.process !== 'yes') return;
+        core.process(options);
+    });
 }());
